978bf87bdd1133a748714c4a4c3506ad4e13ed7a
PAWAN KUMAR JHA <88606641+pjha2002@users.noreply.github.com>
2022-02-26 12:02:55 +0530

    Time: 69 ms (7.05%), Space: 13.6 MB (16.36%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minPartitions(string n) {
+       int m = INT_MIN;
+        for(int i=0;i<n.length();i++){
+            int temp = n[i] - '0';
+            if(temp > m){
+                m = temp;
+            }
+        }
+        return m;
+    }
+};
