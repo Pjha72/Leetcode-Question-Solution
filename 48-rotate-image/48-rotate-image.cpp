7ae29adaf47d7a26c08efa29d72ce82584c59be1
PAWAN KUMAR JHA <88606641+pjha2002@users.noreply.github.com>
2022-03-03 18:42:45 +0530

    Time: 7 ms (15.88%), Space: 7.2 MB (34.09%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,24 @@
+class Solution { 
+public:
+    void transpose(vector<vector<int>>&m){
+        for(int i=0;i<m.size();i++){
+            for(int j=i+1;j<m.size();j++){
+                int temp =m[i][j];
+                m[i][j]=m[j][i];
+                m[j][i]=temp;
+            }
+        }
+    }
+    void rotate(vector<vector<int>>& matrix) {
+        transpose(matrix);
+        for( int i=0;i<matrix.size();i++){
+            for(int j=0;j<(matrix.size())/2;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.size()-1-j];
+                matrix[i][matrix.size()-1-j] = temp;
+            }
+        }
+        
+    }
+   
+};
