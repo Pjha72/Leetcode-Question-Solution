7392e3f9d939e23609acd70eb53732b10eaf84a3
PAWAN KUMAR JHA <88606641+pjha2002@users.noreply.github.com>
2022-03-08 19:07:18 +0530

    Time: 175 ms (75.26%), Space: 93.5 MB (81.25%) - LeetHub

--- 1835-find-xor-sum-of-all-pairs-bitwise-and/1835-find-xor-sum-of-all-pairs-bitwise-and.cpp
+++ 1835-find-xor-sum-of-all-pairs-bitwise-and/1835-find-xor-sum-of-all-pairs-bitwise-and.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int getXORSum(vector<int>& arr1, vector<int>& arr2) {
-        int x1=0,x2=0;
-        for(int i:arr1){
+        int x1=0;
+        int x2=0;
+        for(int i:arr1)
             x1^=i;
-        }
-        for(int i:arr2){
-            x2^=i;
-        }
+        for(int j:arr2)
+            x2^=j;
+        
+        
         return x1&x2;
     }
 };
