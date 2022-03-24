59cf7734b355d080483c351cca719ef0f555b9bd
PAWAN KUMAR JHA <88606641+pjha2002@users.noreply.github.com>
2022-03-24 18:34:45 +0530

    Time: 0 ms (100.00%), Space: 6 MB (87.70%) - LeetHub

--- 917-reverse-only-letters/917-reverse-only-letters.cpp
+++ 917-reverse-only-letters/917-reverse-only-letters.cpp
@@ -1,17 +1,40 @@
 class Solution {
 public:
     string reverseOnlyLetters(string s) {
-        vector<char> v;
-        for(int i=0;i<s.size();i++){
-            if(s[i]>=65 && s[i]<=90 || s[i]>=97 && s[i]<=122){
-                v.push_back(s[i]);
+        // vector<char> v;
+        // for(int i=0;i<s.size();i++){
+        //     if(s[i]>=65 && s[i]<=90 || s[i]>=97 && s[i]<=122){
+        //         v.push_back(s[i]);
+        //     }
+        // }
+        // int j=v.size()-1;
+        // for(int i=0;i<s.size();i++){
+        //     if(s[i]>=65 && s[i]<=90 || s[i]>=97 && s[i]<=122){
+        //         s[i]=v[j];
+        //         j--;
+        //     }
+        // }
+        // return s;
+        
+        int start=0;int end=s.size()-1;
+        while(start<=end){
+            if(s[start]>='a' && s[start]<='z' || s[start]>='A' && s[start]<='Z'){
+                if(s[end]>='a' && s[end]<='z' || s[end]>='A' && s[end]<='Z'){
+                    swap(s[start],s[end]);
+                    start++,end--;
+                }
+                else{
+                    end--;
+                }
             }
-        }
-        int j=v.size()-1;
-        for(int i=0;i<s.size();i++){
-            if(s[i]>=65 && s[i]<=90 || s[i]>=97 && s[i]<=122){
-                s[i]=v[j];
-                j--;
+            else{
+               if(s[end]>='a' && s[end]<='z' || s[end]>='A' && s[end]<='Z'){
+                   start++;
+               }
+                else{
+                    start++;
+                    end--;
+                }  
             }
         }
         return s;
