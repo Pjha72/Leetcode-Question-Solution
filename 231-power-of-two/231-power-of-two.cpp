6120c610609760b4373619a0d680e6e4e9875aca
PAWAN KUMAR JHA <88606641+pjha2002@users.noreply.github.com>
2022-04-11 17:26:10 +0530

    Time: 4 ms (39.58%), Space: 5.8 MB (70.25%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-      if(n<0){
+      if(n==0){
           return false;
       }
-        int bits = __builtin_popcount(n);
-       if(bits==1){
-          return true;
-      }
-        else{
-            return false;
+      //   int bits = __builtin_popcount(n);
+      //  if(bits==1){
+      //     return true;
+      // }
+      //   else{
+      //       return false;
+      //   }
+        
+        while(n%2==0){
+            n/=2;
         }
+        return n==1;
+        
     }
 };
