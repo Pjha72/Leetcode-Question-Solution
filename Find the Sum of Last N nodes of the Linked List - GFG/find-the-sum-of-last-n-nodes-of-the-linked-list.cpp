a1ecda099a3cd04a2b1c80aa4aa2937bbb44900b
PAWAN KUMAR JHA <88606641+pjha2002@users.noreply.github.com>
2022-07-12 20:28:25 +0530

    Added solution - LeetHub

---
+++ Find the Sum of Last N nodes of the Linked List - GFG/find-the-sum-of-last-n-nodes-of-the-linked-list.cpp
@@ -0,0 +1,91 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+};
+
+void append(struct Node** headRef, int newData)
+{
+	struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+	struct Node *last = *headRef;
+	new_node->data = newData;
+	new_node->next = NULL;
+	if (*headRef == NULL)
+	{
+	    *headRef = new_node;
+	    return;
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new_node;
+	return;
+}
+
+int sumOfLastN_Nodes(struct Node* head, int n);
+
+// Driver program to test above
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        struct Node* head = NULL;
+        int n,m,tmp;
+        cin>>m>>n;
+        while(m--){
+            cin>>tmp;
+            append(&head, tmp);
+        }
+        cout<<sumOfLastN_Nodes(head, n)<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*Structure of the node of the linled list is as
+
+struct Node {
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+};
+*/
+// your task is to complete this function 
+// function should return sum of last n nodes
+int sumOfLastN_Nodes(struct Node* head, int n)
+{
+     struct Node* temp = head;
+     int len = 1;
+     while(temp->next!= NULL){
+         temp = temp->next;
+         len++;
+     }
+     
+     if(len == 1) return head->data;
+     if(n == 1) return temp->data;
+     int sum = 0;
+     temp = head;
+     for(int i = 0; i < len - n; i++){
+        temp = temp->next;
+     }
+     do{
+        sum+=temp->data;
+        temp = temp->next; 
+     }while(temp->next!=NULL);
+         sum+=temp->data;
+     
+     return sum;
+}
