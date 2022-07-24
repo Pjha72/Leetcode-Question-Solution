b0340b98f2cf90685719a05c94ee5a7f12ed7c2b
PAWAN KUMAR JHA <88606641+pjha2002@users.noreply.github.com>
2022-07-24 11:29:28 +0530

    Time: 284 ms (76.22%), Space: 118 MB (48.51%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,28 +10,56 @@
  */
 class Solution {
 private:
-    bool checkPalindrome(vector<int> arr){
-        int start = 0, end = arr.size()-1;
-        while(start <= end){
-            if(arr[start] != arr[end]){
-                
-                return false;
-            }
-            start++,end--;
+    ListNode* getMid(ListNode *head){
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        
+        while(fast != NULL && fast->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
         }
-        return true;
+        return slow;
+    }
+    
+    ListNode* reverse(ListNode *head){
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+        
+        while(curr != NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
 public:
     bool isPalindrome(ListNode* head) {
-        // step-1 : create an array
-        vector<int> arr;
-        //step-2 : copy linked list content into an array
-        ListNode* temp = head;
-        while(temp != NULL){
-            arr.push_back(temp->val);
-            temp = temp->next;
+        if(head->next == NULL){
+            return true;
         }
-        // Apply check palindrome logic
-        return checkPalindrome(arr);
+        // step-1 : find the middle element
+        ListNode* middle = getMid(head);
+        //step-2 : reverse the LL after middle element
+        ListNode* temp = middle->next;
+        middle->next = reverse(temp);
+        //step-3 : compare both halves
+        ListNode* head1 = head;
+        ListNode* head2 = middle->next;
+        
+        while(head2 != NULL){
+            if(head1->val != head2->val){
+                return false;
+            }
+            head1 = head1->next;
+            head2 = head2->next;
+        }
+        
+        //step-4 : reverse step-2
+        temp = middle->next;
+        middle->next = reverse(temp);
+        
+        return true;
     }
 };
