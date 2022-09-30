692634622e46ad5f5092cff97000f0a6d75261ac
PAWAN KUMAR JHA <88606641+Pjha72@users.noreply.github.com>
2022-09-30 10:55:39 +0530

    Time: 3 ms (80.04%), Space: 12.9 MB (14.32%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
@@ -15,12 +15,46 @@ public:
         if(root == NULL){
             return false;
         }
-        int ans = root->left->val + root->right->val;
-        if(ans == root->val){
-            return true;
+        // int ans = root->left->val + root->right->val;
+        // if(ans == root->val){
+        //     return true;
+        // }
+        // else{
+        //     return false;
+        // }
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                TreeNode* temp = q.front();
+                q.pop();
+                
+                int parent = temp->val;
+                int child1 = 0;
+                int child2 = 0;
+                
+                if(temp->left){
+                    q.push(temp->left);
+                    child1+=temp->left->val;
+                }
+                else{
+                    child2++;
+                }
+                
+                if(temp->right){
+                    q.push(temp->right);
+                    child1+=temp->right->val;
+                }
+                else{
+                    child2++;
+                }
+                if(child2 == 2) continue;
+                if(parent == child1)    return true;
+            }
         }
-        else{
-            return false;
-        }
+        return false;
     }
 };
