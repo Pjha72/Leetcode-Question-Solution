c0fe57b3ab601788f3c815bf468a82494b4406c9
PAWAN KUMAR JHA <88606641+Pjha72@users.noreply.github.com>
2022-09-30 11:37:36 +0530

    Time: 394 ms (5.16%), Space: 64.6 MB (92.26%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.cpp
+++ 938-range-sum-of-bst/938-range-sum-of-bst.cpp
@@ -10,39 +10,56 @@
  * };
  */
 class Solution {
+private:
+    int sum;
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
-        if(root == NULL){
-            return 0;
-        }
-        int sum = 0;
-        if(root->val<=high && root->val>=low){
-                    sum+=root->val;
-        }
-        queue<TreeNode*> q;
-        q.push(root);
-        
-        while(!q.empty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                TreeNode* temp = q.front();
-                q.pop();
-                
-                
-                if(temp->left){
-                    q.push(temp->left);
-                    if(temp->left->val<=high && temp->left->val>=low){
-                        sum+=temp->left->val;
-                    }
-                }
-                if(temp->right){
-                    q.push(temp->right);
-                    if(temp->right->val<=high && temp->right->val>=low){
-                        sum+=temp->right->val;
-                    }
-                }
-            }
-        }
+        // if(root == NULL){
+        //     return 0;
+        // }
+        sum = 0;
+        traverse(root, low,high);
         return sum;
+        // if(root->val<=high && root->val>=low){
+        //             sum+=root->val;
+        // }
+//         queue<TreeNode*> q;
+//         q.push(root);
+        
+//         while(!q.empty()){
+//             int size = q.size();
+//             for(int i=0;i<size;i++){
+//                 TreeNode* temp = q.front();
+//                 q.pop();
+                
+//                 if(temp->left){
+//                     q.push(temp->left);
+//                     if(temp->left->val<=high && temp->left->val>=low){
+//                         sum+=temp->left->val;
+//                     }
+//                 }
+//                 if(temp->right){
+//                     q.push(temp->right);
+//                     if(temp->right->val<=high && temp->right->val>=low){
+//                         sum+=temp->right->val;
+//                     }
+//                 }
+//             }
+//         }
+//         return sum;
+    }
+    void traverse(TreeNode* root, int low, int high){
+        if(root == NULL){
+            return ;
+        }
+        if(root->val<=high && root->val>=low){
+                sum+=root->val;
+        }
+        if(root->val<=high){
+                traverse(root->right , low, high);
+        }
+        if(root->val >= low){
+              traverse(root->left , low, high);
+        }
     }
 };
