5e7aa352544e1d8931ac7c3b5fca60f47b57a647
PAWAN KUMAR JHA <88606641+Pjha72@users.noreply.github.com>
2023-01-24 10:13:25 +0530

    Added solution - LeetHub

---
+++ Count ways to reach the n'th stair - GFG/count-ways-to-reach-the-nth-stair.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair.
+    int countWays(int n)
+    {
+        if(n<2) return 1;
+        int a = 1,b = 1, c = 0;
+        for(int i=2;i<=n;i++){
+            c = (a + b)%1000000007;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        //taking stair count
+        int m;
+        cin>>m;
+        Solution ob;
+        cout<<ob.countWays(m)<<endl; // Print the output from our pre computed array
+    }
+    return 0;
+}
+
+// } Driver Code Ends
