63aafa84128c30f7987bdbefebc4b84621b8191a
PAWAN KUMAR JHA <88606641+Pjha72@users.noreply.github.com>
2023-02-11 11:40:50 +0530

    Added solution - LeetHub

---
+++ Sort the string in descending order - GFG/sort-the-string-in-descending-order.cpp
@@ -0,0 +1,28 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+string ReverseSort(string str);
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+
+        string s;
+        cin >> s;
+        cout << ReverseSort(s) << endl;
+    }
+return 0;
+}
+
+// } Driver Code Ends
+
+
+
+string ReverseSort(string str){
+    sort(str.begin(),str.end());
+    reverse(str.begin(),str.end());
+    return str;
+    
+}
